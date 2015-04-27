3fce4f1243a378c8005d9524be0734b9dcd795b9
who03 <william.ho96@gmail.com>
2015-04-27 14:53:13 -0700

    Dragging functionality implemented
    However, only works when clicking on the line.

--- graph.js
+++ graph.js
@@ -9,9 +9,10 @@ master = {};
  *  where config contains the information needed to create a plot
  * 
  * config example: 
- * config = {yMax : 40, 
+ * config = {yMax : 10, 
  *           xMax : 10, 
- *           data : [1, 2, 3, 4, 5, 6, 7, 6, 5], 
+ *           data : [1, 2, 3, 4, 5,2,3,4,5,2,7,3,6,8], 
+ *           idata: [0, 1, 2, 3, 4,5,6,7,8,9,10,11,12,13]
  *           width : 500, 
  *           height : 300, 
  *           id : 0}
@@ -25,10 +26,8 @@ master.init = function (config) {
     yMax = config.yMax;
     xMax = config.xMax;
 
-    master[id].xAxisMin = 0; 
-    master[id].xAxisMax = yMax;
     data = config.data;
-    // n = domain max for y axis
+    // n = max amount of data to be displayed on the y axis (domain max for y axis)
     master[id].n = yMax;
     master[id].random = d3.random.normal(xMax/2, .2);
     d3.range(master[id].n);
@@ -36,6 +35,9 @@ master.init = function (config) {
     master[id].data = data;
     master[id].idata = config.idata;
 
+    master[id].xAxisMin = 0; 
+    master[id].xAxisMax = yMax >= d3.max(master[id].idata) ? yMax : d3.max(master[id].idata);
+    
     if (master[id].data === undefined || master[id].idata === undefined) {
         alert('Both data and idata must be defined');
     }
@@ -57,6 +59,16 @@ master.init = function (config) {
     .domain([xMax, 0])
     .range([master[id].height, 0]);
 
+    // Properties for x-axis (tick size, length, etc)
+    master[id].xAxis = d3.svg.axis()
+    .scale(master[id].x)
+    .orient("top");
+    
+    // Properties for y-axis (tick size, length, etc)
+    master[id].yAxis = d3.svg.axis()
+    .scale(master[id].y)
+    .orient("right");
+
     master[id].line = d3.svg.line()
     .x(function(d, i) { return master[id].x(master[id].idata[i]); })
     .y(function(d, i) { return master[id].y(d); });
@@ -68,18 +80,11 @@ master.init = function (config) {
     .append("g")
     .attr("transform", "translate(" + master[id].margin.left + "," + master[id].width + "), rotate(-90)");
 
-    // Creating the clipPath
-    master[id].svg.append("defs").append("clipPath")
-    .attr("id", "clip")
-    .append("rect")
-    .attr("width", master[id].width)
-    .attr("height", master[id].height);
-
     // Creating the x axis and making it pretty
     master[id].svg.append("g")
     .attr("class", "x axis")
     .attr("transform", "translate(-25,0)")
-    .call(d3.svg.axis().scale(master[id].x).orient("top"))
+    .call(master[id].xAxis)
     .selectAll("text")
     .style("text-anchor", "end")
     .attr("dx", "-.8em")
@@ -89,7 +94,7 @@ master.init = function (config) {
     // Creating the y axis and making it pretty
     master[id].svg.append("g")
     .attr("class", "y axis")
-    .call(d3.svg.axis().scale(master[id].y).orient("right"))
+    .call(master[id].yAxis)
     .attr("transform", "translate("+ (master[id].width-25) +")")
     .selectAll("text")
     .style("text-anchor", "end")
@@ -97,6 +102,14 @@ master.init = function (config) {
     .attr("dy", "-1.0em")
     .attr("transform", rotate90);
 
+    // Creating the clipPath
+    master[id].svg.append("clipPath")
+    .attr("id", "clip")
+    .append("rect")
+    .attr("width", master[id].width)
+    .attr("height", master[id].height)
+    .attr("transform", "translate(-25,0)");
+    
     // Adding the data specified in the config
     master[id].path = master[id].svg.append("g")
     .attr("clip-path", "url(#clip)")
@@ -106,57 +119,57 @@ master.init = function (config) {
     .attr("d", master[id].line)
     .attr("transform", "translate(-25,0)");
 
+    // Dragging behavior (zooming not [yet] implemented)
+    master[id].zoom = d3.behavior.zoom()
+        .scaleExtent([1,1])
+        .x(master[id].x)
+        .on("zoom", function() {
+            var t = master[id].zoom.translate(),
+                tx = t[0],
+                ty = t[1]; // Not implemented
+            // Set the max and min bounds of dragging (may need adjustments if new data going down is NEGATIVE instead of current's POSITIVE new data)
+            //  For min: Length of data * Size of tick size in pixels
+            tx = Math.min(tx,(master[id].xAxisMax-master[id].n + 2) * (master[id].width)/(master[id].n - 1));
+            tx = Math.max(tx,0);
+            master[id].zoom.translate([tx,ty]);
+            
+            // Redraw line so it moves
+            master[id].svg.selectAll("path.line").attr("d", master[id].line);
+            // Redraw x axis so it moves
+            master[id].svg.select(".x.axis")
+                .call(master[id].xAxis)
+                .selectAll("text")
+                .style("text-anchor", "end")
+                .attr("dx", "-.8em")
+                .attr("dy", "1.2em")
+                .attr("transform", rotate90);
+        });
+        
+    master[id].svg.call(master[id].zoom);
 }
 
 /**
  * master.tick takes in a data point and an id referring to a created plot --> master.tick(data, id)
  */
 master.tick = function (val, time, pid) {
-    //master[pid].tick(val);
-    
     // push a new data point onto the back
     // data.push(random());
     master[pid].data.push(val);
     master[pid].idata.push(time);
-    // redraw the line, and slide it to the left
-    master[pid].path
-    .attr("d", master[pid].line);
-    // .attr("transform", null)
-    // .transition()
-    // .duration(250)
-    // .ease("linear")
-    // .attr("transform", "translate(" + master[pid].x(-0.1) + ", 0)");
-    // .each("end", tick);
 
     // pop the old data point off the front
     if (time >= master[pid].n) {
-                // redraw the line, and slide it to the left
+                //redraw the line, and slide it to the left
                 master[pid].path
-                .attr("d", master[id].line)
-                .attr("transform", null)
-                .transition()
-                .duration(250)
-                .ease("linear")
-                //.attr("transform", "translate(" + master[pid].x(-0.1) + ", 0)");
-                // .each("end", tick);
-                master[pid].xAxisMin += time - master[pid].n;
-                master[pid].xAxisMax += time - master[pid].n;
+                .attr("d", master[pid].line);
                 
-                //redraws xAxis to increase
-                master[pid].svg.selectAll("g.x.axis")
-                .call(d3.svg.axis().scale(
-                  d3.scale.linear()
-                  .domain([master[pid].xAxisMax - 1, master[pid].xAxisMin])
-                  .range([0, master[pid].width]))
-                .orient("top"))
-                .selectAll("text")
-                .style("text-anchor", "end")
-                .attr("dx", "-.8em")
-                .attr("dy", "1.2em")
-                .attr("transform", rotate90);
-
-        master[pid].data.shift();
-        master[pid].idata.shift();
+                //Change the size of current data for drag to increase
+                master[pid].xAxisMax = d3.max(master[pid].idata);
+    }
+    else {
+        // redraw the line, and slide it to the left
+        master[pid].path
+        .attr("d", master[pid].line);
     }
 }
 
